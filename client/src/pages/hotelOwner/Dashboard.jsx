5c469392c1021b5c192a211f09f02e01a6672a9d
2nas159 <omeraktay159@gmail.com>
2025-06-13 18:38:58 +0300

    first

---
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import Title from "../../components/Title";
+import { assets, dashboardDummyData } from "../../assets/assets";
+
+const Dashboard = () => {
+  const [dashboardData, setDashboardData] = useState(dashboardDummyData);
+
+  return (
+    <div>
+      <Title title="Dashboard" subTitle="Overview of all hotels" />
+
+      <div className="flex gap-4 my-8">
+        {/* Total Bookings */}
+        <div className="bg-blue-500/3 border border-blue-400/10 rounded flex p-4 pr-8">
+          <img
+            src={assets.totalBookingIcon}
+            alt="total-bookings-icon"
+            className="max-sm:hidden h-10"
+          />
+          <div className="flex flex-col sm:ml-4 font-medium">
+            <p className="text-blue-500 text-lg">Total Bookings</p>
+            <p className="text-base text-neutral-400">
+              {dashboardData.totalBookings}
+            </p>
+          </div>
+        </div>
+
+        {/* Total Revenue */}
+        <div className="bg-green-500/3 border border-green-400/10 rounded flex p-4 pr-8">
+          <img
+            src={assets.totalRevenueIcon}
+            alt="total-revenue-icon"
+            className="max-sm:hidden h-10"
+          />
+          <div className="flex flex-col sm:ml-4 font-medium">
+            <p className="text-blue-500 text-lg">Total Revenue</p>
+            <p className="text-base text-neutral-400">
+              $ {dashboardData.totalRevenue}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Recent Bookings */}
+      <h2 className="text-xl font-semibold mb-4">Recent Bookings</h2>
+      <div className="w-full max-w-3xl text-left border border-gray-300 rounded-lg max-h-80 overflow-y-scroll">
+        {/* Table for Recent Bookings */}
+        <table className="w-full">
+          <thead className="bg-gray-50">
+            <tr className="border-b">
+              <th className="py-3 px-4 text-gray-800 font-medium">
+                User Name
+              </th>
+              <th className="py-3 px-4 text-gray-800 font-medium">Room Name</th>
+              <th className="py-3 px-4 text-gray-800 font-medium text-center">
+                Total Amount
+              </th>
+              <th className="py-3 px-4 text-gray-800 font-medium">
+                Payment Status
+              </th>
+            </tr>
+          </thead>
+          <tbody className="text-sm">
+            {dashboardData.bookings.map((item, index) => (
+              <tr key={index} className="border-b hover:bg-gray-50">
+                <td className="py-3 px-4 text-gray-700 border-t border-gray-300">
+                  {item.user.username}
+                </td>
+                <td className="py-3 px-4 text-gray-700 border-t border-gray-300">
+                  {item.room.roomType}
+                </td>
+                <td className="py-3 px-4 text-gray-700 border-t border-gray-300">
+                  $ {item.totalPrice}
+                </td>
+                <td className="py-3 px-4 text-gray-700 border-t border-gray-300">
+                  <button
+                    className={`py-1 px-3 rounded-full mx-auto ${
+                      item.isPaid
+                        ? "bg-green-200 text-green-600"
+                        : "bg-amber-200 text-yellow-600"
+                    }`}
+                  >
+                    {item.isPaid ? "Completed" : "Pending"}
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
